92a6e80a50d81a4ebfdbe6b912a64352f1434718
Sequoia Ploeg <sequoiap4@gmail.com>
2019-11-21 18:27:10 -0700

    Added build, superuser, and start commands

--- server.sh
+++ server.sh
@@ -13,17 +13,31 @@ cd $SERVER
 if [ "$1" == "build" ]
 then
 	# Create and install packages into the virtual environment
-	python3 -m venv env
-	source env/bin/activate
-	pip install -r requirements.txt
+	if [ ! -d ./env ] 
+	then 
+		python3 -m venv env
+		source env/bin/activate
+		pip install -r requirements.txt
+	else
+		source env/bin/activate
+	fi
 
 	# Run migrations on all the apps to build them into the database
-	cd server
-	python manage.py makemigrations machines
-	python manage.py migrate
-	cd ..
+	python server/manage.py makemigrations machines
+	python server/manage.py migrate
 fi
 
 # If not running for build, we assume the virtual environment exists
 source env/bin/activate
 
+if [ "$1" == "createsuperuser" ]
+then
+	python server/manage.py createsuperuser
+fi
+
+# First time builds
+if [ "$1" == "start" ]
+then
+	# Run migrations on all the apps to build them into the database
+	python server/manage.py runserver
+fi
